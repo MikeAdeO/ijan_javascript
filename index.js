9295fbc1c34d2a5a08c27881fd322a0cf6689a58
Ogunlana Michael <ogunlanaadepoju@gmail.com>
2023-05-07 17:54:40 +0100

    javascript class

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+alert("EXTERNAL FILE HERE");
+
+alert("IJAN JAVASCRIPT CLASS"); 
+alert(4 + 7 + 4); */
+
+
+//VARIABLES
+// LET
+let message;
+
+message = "Hello CLASS ";
+let my_name = "MY name is Mike";
+
+console.log(message);
+console.log(my_name);
+
+let buhari_age = 78;
+let tinubu_age = 71;
+let age_difference = buhari_age - tinubu_age;
+
+console.log(age_difference);
+
+// VAR
+
+var bash = "A young man";
+var bash = "A young man";
+console.log(bash);
+
+var bash_1 = "NOTHING ";
+console.log(bash);
+
+
+// CONSTANT
+const myBirthday =" 18 - june - 1960";
+const PLACE_OF_BIRTH = "OGUN STATE";
+
+let numbersofmallinshopright = 67;
+let numbers_of_mall_in_shop_right = 67;
+let numbersOfMallInShopRight = 67;
+
+console.log(myBirthday);
